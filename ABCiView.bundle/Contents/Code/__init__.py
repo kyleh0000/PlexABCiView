e8c0c73163d5b590dc5aebf4b03eeae008c67387
Tim Bell <tim@galacticcode.com>
2013-12-26 12:03:17 +1000

    Formatting code

--- ABCiView.bundle/Contents/Code/__init__.py
+++ ABCiView.bundle/Contents/Code/__init__.py
@@ -3,97 +3,93 @@ from iview_class import *
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
 
+
 def Start():
-	
-	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-	Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+    Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
+    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+
 
 @handler('/video/aubciview', 'Australian ABC iView', art=ART, thumb=ICON)
 def MainMenu():
-    
-	
-	oc = ObjectContainer(view_group='List', title2 = 'ABC iView')
+    oc = ObjectContainer(view_group='List', title2='ABC iView')
 
 
-	#oc.add(VideoClipObject(key = RTMPVideoURL(url = 'rtmp://203.18.195.10/ondemand' + '?auth=7B8F0402DD370FF9299E', clip = 'mp4:comedy/madashell_02_08', swf_url = 'http://www.abc.net.au/iview/images/iview.jpg'), rating_key = '123',title = 'TEST'))
+    #oc.add(VideoClipObject(key = RTMPVideoURL(url = 'rtmp://203.18.195.10/ondemand' + '?auth=7B8F0402DD370FF9299E', clip = 'mp4:comedy/madashell_02_08', swf_url = 'http://www.abc.net.au/iview/images/iview.jpg'), rating_key = '123',title = 'TEST'))
 
-	cats = iView_Config.List_Categories()
-	
-	for key in cats:
-		 	
-		oc.add(DirectoryObject(
-				key = Callback(GetSeriesByCaegory, category=key),
-				title = cats[key]
-				))
-	
-	oc.objects.sort(key = lambda obj: obj.title)
-	
-	return oc
+    cats = iView_Config.List_Categories()
+
+    for key in cats:
+        oc.add(DirectoryObject(
+            key=Callback(GetSeriesByCaegory, category=key),
+            title=cats[key]
+        ))
+
+    oc.objects.sort(key=lambda obj: obj.title)
+
+    return oc
+
 
 @route('/video/aubciview/series/{category}')
 def GetSeriesByCaegory(category):
-	
-	cat = iView_Category(category)
-	
-	oc = ObjectContainer(view_group='List', title2 = cat.title)
-	
-	series = cat.series_list
-	
-	for item in series:
+    cat = iView_Category(category)
 
-		oc.add(DirectoryObject(
-				key = Callback(GetEpisodesBySeries, series=item[0]),
-				title = item[1]
-				))
-	oc.objects.sort(key = lambda obj: obj.title)
-	
-	return oc
-	
+    oc = ObjectContainer(view_group='List', title2=cat.title)
+
+    series = cat.series_list
+
+    for item in series:
+        oc.add(DirectoryObject(
+            key=Callback(GetEpisodesBySeries, series=item[0]),
+            title=item[1]
+        ))
+    oc.objects.sort(key=lambda obj: obj.title)
+
+    return oc
+
+
 @route('/video/aubciview/episode/{series}')
 def GetEpisodesBySeries(series):
-	
-	
-	
-	show = iView_Series(series)
-	
-	oc = ObjectContainer(view_group='InfoList', title2 = show.title, no_cache = True)
-	
-	episodes = show.episodes
-	
-	rtmp_url = iView_Config.RTMP_URL()
-	
-	for item in episodes:
-		
-		oc.add(Play_iView(item[1], item[2], item[3], item[4], item[5], rtmp_url))
-	
-	oc.objects.sort(key = lambda obj: obj.title)
-	
-	return oc
+    show = iView_Series(series)
+
+    oc = ObjectContainer(view_group='InfoList', title2=show.title, no_cache=True)
+
+    episodes = show.episodes
+
+    rtmp_url = iView_Config.RTMP_URL()
+
+    for item in episodes:
+        oc.add(Play_iView(item[1], item[2], item[3], item[4], item[5], rtmp_url))
+
+    oc.objects.sort(key=lambda obj: obj.title)
+
+    return oc
+
 
 @route('/video/aubciview/episode/play')
 def Play_iView(iView_Title, iView_Summary, iView_Path, iView_Thumb, iView_Duration, video_url, include_container=False):
-	
-	HTTP.ClearCache()
-	
-	vco = VideoClipObject(
-		key = Callback(Play_iView, iView_Title=iView_Title, iView_Summary=iView_Summary, iView_Path=iView_Path, iView_Thumb=iView_Thumb, iView_Duration=iView_Duration, include_container=True),
-		rating_key = iView_Path,
-		title = iView_Title,
-		summary = iView_Summary,
-		thumb = iView_Thumb,
-		duration = iView_Duration,
-		items = [
-			MediaObject(
-				parts = [
-					PartObject(
-						key = RTMPVideoURL(url = video_url, clip = iView_Config.CLIP_PATH() + iView_Path, swf_url = iView_Config.SWF_URL)
-					)
-				]
-			)
-		]
-	)
+    HTTP.ClearCache()
 
-	if include_container:
-		return ObjectContainer(objects=[vco])
-	else:
-		return vco
+    vco = VideoClipObject(
+        key=Callback(Play_iView, iView_Title=iView_Title, iView_Summary=iView_Summary, iView_Path=iView_Path,
+                     iView_Thumb=iView_Thumb, iView_Duration=iView_Duration, include_container=True),
+        rating_key=iView_Path,
+        title=iView_Title,
+        summary=iView_Summary,
+        thumb=iView_Thumb,
+        duration=iView_Duration,
+        items=[
+            MediaObject(
+                parts=[
+                    PartObject(
+                        key=RTMPVideoURL(url=video_url, clip=iView_Config.CLIP_PATH() + iView_Path,
+                                         swf_url=iView_Config.SWF_URL)
+                    )
+                ]
+            )
+        ]
+    )
+
+    if include_container:
+        return ObjectContainer(objects=[vco])
+    else:
+        return vco
--- ABCiView.bundle/Contents/Code/iview_class.py
+++ ABCiView.bundle/Contents/Code/iview_class.py
@@ -1,114 +1,111 @@
 class iView_Config():
+    BASE_URL = 'http://www.abc.net.au/iview/'
 
-	BASE_URL = 'http://www.abc.net.au/iview/'
-		
-	CFG_URL = BASE_URL + 'xml/config.xml'
-	CFG_XML = XML.ElementFromURL(CFG_URL)
-		
-	AUTH_URL = CFG_XML.xpath('/config/param[@name="auth"]')[0].get("value")
-	API_URL = CFG_XML.xpath('/config/param[@name="api"]')[0].get("value")
-		
-	CAT_URL = BASE_URL + CFG_XML.xpath('/config/param[@name="categories"]')[0].get("value")
-	
-	RTMP_Server = CFG_XML.xpath('/config/param[@name="server_streaming"]')[0].get("value") + '?auth='
-	SWF_URL = 'http://www.abc.net.au/iview/images/iview.jpg'
-		
-	CAT_XML = XML.ElementFromURL(CAT_URL)
-	SERIES_URL = API_URL + 'seriesIndex'
-	SERIES_JSON = JSON.ObjectFromURL(SERIES_URL)
-	category_list = {}
-	
-	@classmethod
-	def RTMP_URL(self):
-	
-		xml = XML.ElementFromURL(url=self.AUTH_URL)
-		token = xml.xpath('//a:token/text()', namespaces={'a': 'http://www.abc.net.au/iView/Services/iViewHandshaker'})[0]
-		return xml.xpath('//a:server/text()', namespaces={'a': 'http://www.abc.net.au/iView/Services/iViewHandshaker'})[0] + '?auth=' + token
-	
-	@classmethod
-	def CLIP_PATH(self):
-		xml = XML.ElementFromURL(self.AUTH_URL)
-		path = xml.xpath('//a:path/text()', namespaces={'a': 'http://www.abc.net.au/iView/Services/iViewHandshaker'})
-		if not path:
-			return 'mp4:'
-		
-		return 'mp4:' + path[0]
-		
-	@classmethod
-	def List_Categories(self):
-		 cats = {}
-		 for cat in self.CAT_XML.xpath('/categories/category'):
-		 	 id = cat.get('id')
-		 	 name = cat.find('name').text
-		 	 if id in ['test', 'index']:
-		 	 	continue
-		 	 cats[id] = name
-		 
-		 return cats
-	
-	
-	
+    CFG_URL = BASE_URL + 'xml/config.xml'
+    CFG_XML = XML.ElementFromURL(CFG_URL)
+
+    AUTH_URL = CFG_XML.xpath('/config/param[@name="auth"]')[0].get("value")
+    API_URL = CFG_XML.xpath('/config/param[@name="api"]')[0].get("value")
+
+    CAT_URL = BASE_URL + CFG_XML.xpath('/config/param[@name="categories"]')[0].get("value")
+
+    RTMP_Server = CFG_XML.xpath('/config/param[@name="server_streaming"]')[0].get("value") + '?auth='
+    SWF_URL = 'http://www.abc.net.au/iview/images/iview.jpg'
+
+    CAT_XML = XML.ElementFromURL(CAT_URL)
+    SERIES_URL = API_URL + 'seriesIndex'
+    SERIES_JSON = JSON.ObjectFromURL(SERIES_URL)
+    category_list = {}
+
+    @classmethod
+    def RTMP_URL(self):
+
+        xml = XML.ElementFromURL(url=self.AUTH_URL)
+        token = xml.xpath('//a:token/text()', namespaces={'a': 'http://www.abc.net.au/iView/Services/iViewHandshaker'})[
+            0]
+        return xml.xpath('//a:server/text()', namespaces={'a': 'http://www.abc.net.au/iView/Services/iViewHandshaker'})[
+                   0] + '?auth=' + token
+
+    @classmethod
+    def CLIP_PATH(self):
+        xml = XML.ElementFromURL(self.AUTH_URL)
+        path = xml.xpath('//a:path/text()', namespaces={'a': 'http://www.abc.net.au/iView/Services/iViewHandshaker'})
+        if not path:
+            return 'mp4:'
+
+        return 'mp4:' + path[0]
+
+    @classmethod
+    def List_Categories(self):
+        cats = {}
+        for cat in self.CAT_XML.xpath('/categories/category'):
+            id = cat.get('id')
+            name = cat.find('name').text
+            if id in ['test', 'index']:
+                continue
+            cats[id] = name
+
+        return cats
+
+
 class iView_Series(object):
-	
-	def __init__(self, key):
-		
-		self.id = key
-		json = JSON.ObjectFromURL(iView_Config.API_URL + 'series=' + key)
-		
-		self.title = json[0]['b']
-		self.description = json[0]['c']
-		self.category = json[0]['e']
-		self.img = json[0]['d']
-		self.episode_count = len(json[0]['f'])
-		self.episodes = self.Episodes(json[0]['f'])
-		
-	def Episodes(self, json):
-		eps = []
-		for ep in json:
-			id = ep['a']
-			title = ep['b']
-			description = ep['d']
-			url = ep['n'][:-4]
-			thumb = ep['s']
-			duration = int(ep['j'])
-			tmp = []
-			tmp.append(id)
-			tmp.append(title)
-			tmp.append(description)
-			tmp.append(url)
-			tmp.append(thumb)
-			tmp.append(duration)
-			eps.append(tmp)
-			
-		return eps
+    def __init__(self, key):
+        self.id = key
+        json = JSON.ObjectFromURL(iView_Config.API_URL + 'series=' + key)
 
-	
+        self.title = json[0]['b']
+        self.description = json[0]['c']
+        self.category = json[0]['e']
+        self.img = json[0]['d']
+        self.episode_count = len(json[0]['f'])
+        self.episodes = self.Episodes(json[0]['f'])
+
+    def Episodes(self, json):
+        eps = []
+        for ep in json:
+            id = ep['a']
+            title = ep['b']
+            description = ep['d']
+            url = ep['n'][:-4]
+            thumb = ep['s']
+            duration = int(ep['j'])
+            tmp = []
+            tmp.append(id)
+            tmp.append(title)
+            tmp.append(description)
+            tmp.append(url)
+            tmp.append(thumb)
+            tmp.append(duration)
+            eps.append(tmp)
+
+        return eps
+
+
 class iView_Category(object):
-	
-	def __init__(self, key):
-	
-		self.id = key
-		cats = iView_Config.List_Categories()
-		self.title = cats[key]
-		self.series_list = self.Series(key)
-	
-	def Series(self, search):
-		series = []
-		for show in iView_Config.SERIES_JSON:
-			id = show['a']
-			title = show['b']
-			category = show['e']
-			count = len(show['f'])
-			tmp = []
-			tmp.append(id)
-			tmp.append(title)
-			tmp.append(category)
-			tmp.append(count)
-			
-			if category.find(search) >= 0:
-				series.append(tmp)
-			
-		return series
+    def __init__(self, key):
+
+        self.id = key
+        cats = iView_Config.List_Categories()
+        self.title = cats[key]
+        self.series_list = self.Series(key)
+
+    def Series(self, search):
+        series = []
+        for show in iView_Config.SERIES_JSON:
+            id = show['a']
+            title = show['b']
+            category = show['e']
+            count = len(show['f'])
+            tmp = []
+            tmp.append(id)
+            tmp.append(title)
+            tmp.append(category)
+            tmp.append(count)
+
+            if category.find(search) >= 0:
+                series.append(tmp)
+
+        return series
 	
 	
 	
